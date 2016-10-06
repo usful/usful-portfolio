adb14da4d49c5e01f64b5edbc8d40a35cc19c312
Rishabh Nag <rishabhnag@Rishabhs-MacBook-Air.local>
2016-10-06 12:15:07 -0400

    Reverting usfulportfolio file to avoid merge conflicts

--- src/UsfulPortfolio.js
+++ src/UsfulPortfolio.js
@@ -1,43 +1,25 @@
 import React, { Component } from 'react';
 import {
-    StyleSheet,
-    Text,
-    View,
-    ListView,
-ScrollView
+  StyleSheet,
+  Text,
+  View,
+  ListView
 } from 'react-native';
 
 import AppData from './AppData';
 
 import DetailedStoryItem from './components/Story/DetailedStoryItem';
-
-import ProductView from './components/Product/ProductView';
-import InitiativeView from './components/Initiative/InitiativeView';
-import global from './styles';
-
 import Portfolio from './components/Portfolio';
 import TagView from './components/TagView';
-import TopNav from './components/TopNav';
 
 export default class UsfulPortfolio extends Component {
   constructor(props) {
     super(props);
   }
 
-  componentDidMount() {
-    console.log(AppData.products[1].address);
-}
   render() {
     return (
-
-      <ScrollView style= {global.container}>
-        <TopNav />
-        <ProductView products={AppData.products}/>
-      </ScrollView>
+      <DetailedStoryItem story={AppData.stories[0]} nextStory={AppData.stories[1]}/>
     );
-
-
-
-
   }
 }
