673477f9dcab2ddb236553a3ec23621d5ccecb05
Tsz Nau Mercedes Lo <mercedes@Clintons-MacBook-Pro-3.local>
2016-10-18 09:50:11 -0400

    index ios

--- index.ios.js
+++ index.ios.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import {AppRegistry} from 'react-native';
 
 import UsfulPortfolio from './src/UsfulPortfolio';
-import EmailInput from './src/components/scenes/IntroductionScene';
-import Portfolio from './src/components/scenes/PortfolioScene';
+import IntroductionScene from './src/components/scenes/IntroductionScene';
+import PortfolioScene from './src/components/scenes/PortfolioScene';
+import Team from './src/components/Team';
+import SplashScene from './src/components/scenes/SplashScene';
 
-AppRegistry.registerComponent('UsfulPortfolio', () => Portfolio);
+AppRegistry.registerComponent('UsfulPortfolio', () => Team);
