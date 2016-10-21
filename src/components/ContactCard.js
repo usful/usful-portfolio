aaa6251043d3bd12b701567d9745baefc52cec5d
Tsz Nau Mercedes Lo <mercedes@Clintons-MacBook-Pro-3.local>
2016-10-21 14:06:46 -0400

    Contact Cards UI fix

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -7,8 +7,8 @@ import {
     Text,
     Linking,
     ScrollView,
-    TouchableHighlight,
-  TouchableOpacity,
+    AlertIOS,
+    TouchableOpacity,
     View,
 } from 'react-native';
 
@@ -19,20 +19,35 @@ import Colours from '../styles/Colours';
 import Font from '../styles/Font';
 
 let styles = StyleSheet.create({
+  linearGradient: {
+    backgroundColor: "transparent",
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0
+  },
+  backgroundImage: {
+    marginHorizontal:0,
+    width: 250, height: 300,
+    alignItems: 'center',
+    resizeMode: 'contain',
+    justifyContent: 'center',
+
+  },
   container: {
     alignItems: 'center',
     flex: 1,
     marginHorizontal: -50,
-    backgroundColor: 'transparent'
+    backgroundColor: 'white'
 
   },
   card: {
-    width: 250,
-    height: 420,
+    width: 350,
+    height: 480,
     paddingVertical: 30,
-    backgroundColor: Colours.white,
+    backgroundColor: 'white',
     alignItems: 'center',
-    marginHorizontal: -0,
   },
   cardImage: {
     borderWidth: 1,
@@ -44,39 +59,50 @@ let styles = StyleSheet.create({
   },
   page: {
     color: Colours.darkGrey,
-    fontFamily: Font.primaryFont.fontFamily
+    fontFamily: Font.primaryFont.fontFamily,
+    fontWeight: 'bold'
+
 
   },
   close: {
-    paddingLeft: 120,
+    paddingLeft: 140,
     color: Colours.darkGrey,
-    fontFamily: Font.primaryFont.fontFamily
+    fontFamily: Font.primaryFont.fontFamily,
+    fontWeight: 'bold'
   },
   contactInfo: {
-    alignItems: 'center'
+    alignItems: 'center',
+    marginHorizontal: 20,
+    justifyContent: 'center',
+    backgroundColor: 'white'
   },
   pageAndClose: {
+    marginLeft: 20,
+    marginTop: 20,
     flexDirection: 'row',
-    position: 'absolute',
-    top: 20,
-    left: 20,
+
+    backgroundColor: 'white',
+
   },
   textDescription: {
     color: Colours.darkGrey,
+    width: 100,
     fontSize: 12,
     lineHeight: 15,
-    marginHorizontal: 25,
     paddingTop: 10,
+    paddingBottom:20,
     fontFamily: Font.primaryFont.fontFamily,
-    alignItems: 'stretch'
+    textAlign: 'center'
+
 
   },
   textName: {
-    marginTop: 30,
+    marginTop: 20,
     fontWeight: '600',
     color: Colours.darkGrey,
     fontSize: 18,
-    fontFamily: Font.fontFamily
+    fontFamily: Font.primaryFont.fontFamily,
+    textAlign: 'center'
   },
 
   textTags: {
@@ -86,18 +112,19 @@ let styles = StyleSheet.create({
     fontSize: 8,
     paddingTop: 5,
     fontFamily: Font.primaryFont.fontFamily,
+    textAlign: 'center'
 
   },
   socialMediaBox: {
     flexDirection: 'row',
-    backgroundColor: Colours.darkGrey,
+    backgroundColor: 'transparent',
     width: 250,
     paddingVertical: 10,
     justifyContent: 'center'
   },
   mediaIcon: {
-    width: 30,
-    height: 30,
+    width: 40,
+    height: 40,
     margin: 5,
 
   }
@@ -113,50 +140,102 @@ export default class ContactCard extends Component {
 
   constructor(props) {
     super(props);
+    let iconA = new Animated.Value(1);
+    let iconB = new Animated.Value(1);
+    let iconC = new Animated.Value(1);
+    this.state = {
+      animIcons: [iconA, iconB, iconC],
+    }
   }
 
-  openMedia(url) {
-    console.log("open");
-    Linking.canOpenURL(url).then(supported => {
-      if (!supported) {
-        console.log('Can\'t handle url: ' + url);
-      } else {
-        return Linking.openURL(url);
-      }
-    }).catch(err => console.error('An error occurred', err));
+  componentDidMount(){
+    this.getMediaIcon();
+  }
+
+  getMediaIcon(type){
+    switch(type) {
+      case 'instagram':
+        return require('../../assets/instagram.png');
+        break;
+      case 'github':
+        return require('../../assets/github.png');
+        break;
+      case 'twitter':
+        return require('../../assets/twitter.png');
+        break;
+      case 'linkedin':
+        return require('../../assets/linkedin.png');
+        break;
+      case 'googleplus':
+        return require('../../assets/googleplus.png');
+        break;
+      default:
+        return require('../../assets/youtube.png');
+    }
+
+  }
+
+  openMedia(url,i) {
+    Animated.spring( this.state.animIcons[i], {toValue: 0.8, friction: 1}).start();
+    AlertIOS.alert(
+      'Leave Usful',
+      'Clicking OK will take you to safari',
+      [
+        {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+        {text: 'OK', onPress: () => Linking.canOpenURL(url)
+          .then(supported => {
+          if (!supported) {
+            console.log('Can\'t handle url: ' + url);
+          } else {
+            return Linking.openURL(url);
+          }
+        }).catch(err => console.error('An error occurred', err))},
+      ],
+    );
+
   }
 
   render() {
     let person = this.props.person;
-
+    let source;
+//modal to external site
+    //bg img
     return (
-      <View style={[styles.container]}>
-        <LinearGradient colors={['white', 'transparent']}
-                        start={[0,0.5]} end={[0,0]}
-                        location={[0.5,0.5,0.5]}>
-          <View style={[styles.card]}>
+      <View>
+        <View style={{backgroundColor:'white'}}>
           <View style={styles.pageAndClose}>
             <Text style={styles.page}>{this.props.index}/{this.props.totalCards}</Text>
             <TouchableOpacity><Text style={styles.close}>CLOSE</Text></TouchableOpacity>
           </View>
-          <Image source={{uri: person.picture}} style={styles.cardImage}/>
+          <View style={{height:200,width:200}} >
+          </View>
+
+
+
+          <LinearGradient
+            colors={['green', 'red']}
+            start={[0.0, 0.5]} end={[1.0, 0.5]} locations={[0.0, 1.0]}></LinearGradient>
+
+
           <View style={styles.contactInfo}>
             <Text style={styles.textName}>{person.name}</Text>
             <Text style={styles.textTags}>{person.tags.join(' / ')}</Text>
             <Text style={styles.textDescription}>{person.description}</Text>
           </View>
 
+
         </View>
-        </LinearGradient>
+
         <View style={styles.socialMediaBox}>
 
-          <TouchableOpacity onPress={()=> this.openMedia(person.socialAccounts[0].uri)}><Image source={require('../../assets/Instagram.png')}  style={styles.mediaIcon}/></TouchableOpacity>
-          <TouchableOpacity onPress={()=>this.openMedia(person.socialAccounts[1].uri)}><Image source={require('../../assets/facebook.png')} style={styles.mediaIcon}/></TouchableOpacity>
+          {person.socialAccounts.map((acct,i) => <TouchableOpacity key={i} style={{transform: [{scale: this.state.animIcons[i]}]}}
+                                                                            onPress={()=> this.openMedia(acct.uri,i)}>
+                                                  <Image source={this.getMediaIcon(acct.type)} style={styles.mediaIcon}/>
+                                                </TouchableOpacity>)}
 
-          <TouchableOpacity onPress={()=>this.openMedia(person.socialAccounts[2].uri)}><Image source={require('../../assets/linkedin.png')} style={styles.mediaIcon}/></TouchableOpacity>
 
+          </View>
         </View>
-      </View>
     );
   }
 }
