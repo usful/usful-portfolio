6d313fdac75f1c5c18a73da9a10027fe26bf1e61
Rishabh Nag <rishabhnag@Rishabhs-MacBook-Air.local>
2016-10-26 11:02:08 -0400

    Moved contact footer under blocks and made it appearonly when person scrolls to bottom of blocks

--- src/components/Content/DetailedContentItem.js
+++ src/components/Content/DetailedContentItem.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
 import {
-  StyleSheet,
-  Text,
-  ScrollView,
-  View
+    StyleSheet,
+    Text,
+    ScrollView,
+    Dimensions,
+    View
 } from 'react-native';
 
 import global from '../../styles';
@@ -21,6 +22,8 @@ import SliderItem from './SliderItem';
 import ImageItem from './ImageItem';
 import NextContentButton from './NextContentButton';
 
+let {width,height} = Dimensions.get('window');
+
 export default class DetailedContentItem extends Component {
 
   static defaultProps = {
@@ -30,6 +33,18 @@ export default class DetailedContentItem extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      footerToggle : false
+    }
+  }
+
+  _handleScroll(el, blockHeight) {
+    let scrollHeight = el.nativeEvent.contentOffset.y + height;
+    if (scrollHeight === blockHeight) {
+      this.setState({
+        footerToggle: !this.state.footerToggle
+      });
+    }
   }
 
   renderFooter() {
@@ -48,46 +63,77 @@ export default class DetailedContentItem extends Component {
   render() {
     let blocks = this.props.content.blocks;
     let type = this.props.content.type;
-
+    let blockHeight = 0;
     return (
-      <ScrollView style={global.container}>
 
-        <HeaderItem image={this.props.content.header.uri} date={longDateFormatter(this.props.content.date)}/>
-        <TitleItem content={this.props.content} title={this.props.content.name} tags={this.props.content.tags}/>
+      <ScrollView
+        alwaysBounceVertical = {true}
+        ref={ref => this._ScrollView = ref}
+        scrollEventThrottle={16}
+        onScroll= {(el) => this._handleScroll(el, blockHeight)} style={[global.container]}>
+        <View style = {this.state.footerToggle ? styles.contactShow : styles.contactHide}>
+          {this.renderFooter()}
+        </View>
+        <View
+          onLayout={(event) => {
+            blockHeight = event.nativeEvent.layout.height;
+          }}
+          style = {type !== 'Story' && this.state.footerToggle ? styles.contentShow : styles.contentHide}>
+          <HeaderItem image={this.props.content.header.uri} date={longDateFormatter(this.props.content.date)}/>
+          <TitleItem content={this.props.content} title={this.props.content.name} tags={this.props.content.tags}/>
+  
+          {blocks.map((block, index) => {
+            switch (block._type) {
+              case 'CopyBlock':
+                return <BodyItem key={index} text={block.text}/>;
+              case 'LegalBlock':
+                return <BodyItem style={styles.legal} key={index} text={block.text}/>;
+              case 'MediaBlock':
+                return <ImageItem key={index} image={block.media.uri}/>;
+              case 'MediaCarouselBlock':
+                return <SliderItem key={index}
+                                   images={block.media.map((img) => img.uri)}/>;
+              case 'QuoteBlock':
+                return <FeatureImageItem image={block.media.uri}
+                                         key={index}
+                                         author={block.author}
+                                         byline={block.text}/>;
+              default:
+                return <View key={index}><Text>{block._type}</Text></View>;
+            }
+          })}
 
-        {blocks.map((block, index) => {
-          switch (block._type) {
-            case 'CopyBlock':
-              return <BodyItem key={index} text={block.text}/>;
-            case 'LegalBlock':
-              return <BodyItem style={styles.legal} key={index} text={block.text}/>;
-            case 'MediaBlock':
-              return <ImageItem key={index} image={block.media.uri}/>;
-            case 'MediaCarouselBlock':
-              return <SliderItem key={index}
-                                 images={block.media.map((img) => img.uri)}/>;
-            case 'QuoteBlock':
-              return <FeatureImageItem image={block.media.uri}
-                                       key={index}
-                                       author={block.author}
-                                       byline={block.text}/>;
-            default:
-              return <View key={index}><Text>{block._type}</Text></View>;
-          }
-        })}
 
-        <NextContentButton style={type === 'Story' ? styles.noShadow : styles.shadow} content={this.props.nextContent}
-                           image={this.props.content.footer.uri}/>
-
-        {this.renderFooter()}
+          <NextContentButton style={type === 'Story' ? styles.noShadow : styles.shadow} content={this.props.nextContent}
+                             image={this.props.content.footer.uri}/>
+        </View>
 
         <CloseButton onPress={() => Navigation.popToRoute(Navigation.PORTFOLIO_SCENE)}/>
+
       </ScrollView>
+
     );
   }
 }
 
 const styles = StyleSheet.create({
+  contactShow: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    height: ContactFooter.FOOTER_HEIGHT,
+    opacity: 1
+  },
+  contactHide: {
+    height: 0,
+    opacity: 0
+  },
+  contentShow: {
+    marginBottom: ContactFooter.FOOTER_HEIGHT - ContactFooter.UNDERLAY_HEIGHT
+  },
+  contentHide: {
+    marginBottom: 0
+  },
   legal: {
     fontFamily: 'Avenir-BookOblique',
     fontSize: 12,
@@ -96,6 +142,7 @@ const styles = StyleSheet.create({
     color: Colors.textGrey
   },
   shadow: {
+    elevation: 3,
     shadowOffset: {
       height: 6,
       width: 6
