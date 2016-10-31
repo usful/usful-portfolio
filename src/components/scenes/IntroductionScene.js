af5342299d96ce15234200f5057b2cf78bb3bc97
Mercedes Lo <mersaythis@gmail.com>
2016-10-31 16:44:35 -0400

    ui for diff scr sizes

--- src/components/scenes/IntroductionScene.js
+++ src/components/scenes/IntroductionScene.js
@@ -22,29 +22,21 @@ import Typewriter from '../../Typewriter';
 import Navigation from '../../helpers/Navigation';
 
 let {width, height} = Dimensions.get('window');
-let introMsg = `Planetary change is inevitable. Through technology, design, and education Usful is preparing communities for this reality. We are a conscious team of technologists, designers, developers, engineers, and architects who create Usful products with purpose.`;
+let introMsg = `Planetary change is inevitable. Through technology, design, and education. Usful is preparing communities for this reality. We are a conscious team of technologists, designers, developers, engineers, and architects who create Usful products with purpose.`;
 let okMsg = `Perfect. You will be receiving a package from the Usful team momentarily.`;
 
 let styles = StyleSheet.create({
 
   container: {
-    alignItems: 'flex-start',
-    justifyContent: 'center',
-    marginLeft: 30,
-    padding: 0,
-    backgroundColor:  Colours.white,
+    justifyContent: 'flex-end',
+    backgroundColor: 'transparent',
+    marginTop: 100,
+    marginLeft: 30
   },
-  emailInput: {
-    width: width-70,
-    color: Colours.white,
-    borderColor:  Colours.white,
-    borderWidth: 1,
-    alignItems: 'center',
-    marginTop: height/6,
-    marginRight: 20,
-    paddingHorizontal: 20,
-    height: 55,
-    opacity: 0.7
+   enter: {
+     marginTop: 30,
+     alignSelf: 'flex-end' ,
+     marginRight: 40
   },
   enterOurWorld: {
     alignItems: 'center',
@@ -55,10 +47,6 @@ let styles = StyleSheet.create({
     marginTop: 150,
     width: width-60,
   },
-  errorEmail:{
-    color: Colours.white,
-  },
-
   font: {
     color:  Colours.white,
     fontFamily: Font.secondaryFont.fontFamily,
@@ -67,9 +55,7 @@ let styles = StyleSheet.create({
   },
   introMsgContainer: {
     backgroundColor:  Colours.navBarBlack,
-    marginTop: 120,
-    marginHorizontal: 30,
-    marginBottom: 120,
+
   },
   invalidText: {
     color:  Colours.white,
@@ -79,42 +65,18 @@ let styles = StyleSheet.create({
   msg: {
     color:  Colours.white,
     fontFamily: Font.primaryFont.fontFamily,
-    fontSize: 21,
+    fontSize: 18,
     fontWeight: '400',
-    lineHeight:28,
+    lineHeight:23,
   },
   skip:{
     alignSelf:'flex-end',
     color:  Colours.white,
     fontSize: 20,
-    marginTop: 40,
-    paddingRight:20,
-  },
-  upperView:{
-    zIndex: 12,
-  },
-  underView:{
-    zIndex: 2,
-  },
-  validText: {
-    color:  Colours.navBarBlack,
+
   },
   view :{
     backgroundColor:  Colours.navBarBlack,
-    height: height,
-    zIndex: 0
-  },
-  viewTop:{
-    backgroundColor: Colours.transparent,
-    height: height,
-    position:'absolute',
-    zIndex:12,
-  },
-  viewBottom:{
-    backgroundColor: Colours.transparent,
-    height: height,
-    position:'absolute',
-    zIndex: 2
   },
 });
 
@@ -168,20 +130,16 @@ export default class IntroductionScene extends Component {
 
   render() {
     return (
-      <View style={styles.view}>
-        <View style={styles.viewTop}>
-          <KeyboardHandler ref='kh' offset={100}>
-            <View style={styles.introMsgContainer}>
-            <Typewriter ref="introMsg" style={[styles.msg]} msg={introMsg} colour={'white'} speed={300} space={10}/>
+        <ScrollView style={styles.view}>
+              <View style={styles.container}>
+                <Typewriter ref="introMsg" style={[styles.msg]} msg={introMsg} colour={'white'} speed={300} space={10}/>
+                <TouchableOpacity style={styles.enter} onPress={() => Navigation.push(Navigation.PORTFOLIO_SCENE)}>
+                  <Animated.Text style={[styles.font, styles.skip,{opacity: this.state.emailFadeIn}]}>ENTER</Animated.Text>
+                </TouchableOpacity>
+              </View>
+        </ScrollView>
 
-              <TouchableOpacity style={{marginTop: Platform.OS === 'ios'? 440: 400, marginRight: -10}} onPress={() => Navigation.push(Navigation.PORTFOLIO_SCENE)}>
-                <Animated.Text style={[styles.font, styles.skip,{opacity: this.state.emailFadeIn}]}>ENTER</Animated.Text>
-              </TouchableOpacity>
-            </View>
 
-          </KeyboardHandler>
-        </View>
-      </View>
     );
   }
 }
